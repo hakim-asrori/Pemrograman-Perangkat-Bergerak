ff94c5cf2afa3d72dbaf88652588e51117467039
Hakim Asrori <77718626+hakim-asrori@users.noreply.github.com>
2022-02-22 18:56:05 +0700

    Finishing tugas

--- Tugas Praktikum 17 Februari 2022/tugas/percobaan_1.dart
+++ Tugas Praktikum 17 Februari 2022/tugas/percobaan_1.dart
@@ -1,63 +1,74 @@
 import 'dart:io';
 
-void statis() {
-  Map data = {
-    0: {
-      "nim": "09030015",
-      "nama": "Udin",
-      "matkul": <int, String>{
-        0: "PW",
-        1: "PBO",
-        2: "PPB",
-      }
-    },
-    1: {
-      "nim": "09030016",
-      "nama": "Joko",
-      "matkul": <int, String>{
-        0: "Basdat",
-        1: "RPL",
-      }
-    },
-  };
+// void statis() {
+//   Map data = {
+//     0: {
+//       "nim": "09030015",
+//       "nama": "Udin",
+//       "matkul": <int, String>{
+//         0: "PW",
+//         1: "PBO",
+//         2: "PPB",
+//       }
+//     },
+//     1: {
+//       "nim": "09030016",
+//       "nama": "Joko",
+//       "matkul": <int, String>{
+//         0: "Basdat",
+//         1: "RPL",
+//       }
+//     },
+//   };
 
-  print(data);
-}
+//   print(data);
+// }
 
 void dinamis() {
-  List<dynamic> namaMatkul =  <dynamic>[];
+  // Deklarasi Variable
+  var namaMatkul =  new Map();
   var dataMahasiswa = new Map();
-
-  stdout.write("Masukan Jumlah Mahasiswa : ");
-  int? jumlahMahasiswa = int.parse(stdin.readLineSync()!);
-
+  
   String? nim;
   String? nama;
   
+  // Memasukan jumlah mahasiswa
+  stdout.write("Masukan Jumlah Mahasiswa : ");
+  int? jumlahMahasiswa = int.parse(stdin.readLineSync()!); // Konversi dari String ke int
+  
+  // Looping jumlah mahasiswa
   for(int i = 0; i < jumlahMahasiswa; i++) {
+    // Memasukan nim, nama mahasiswa
     stdout.write("Masukan NIM : ");
-    nim = stdin.readLineSync();
+    nim = stdin.readLineSync(); // data input dimasukan ke variable nim
     stdout.write("Masukan Nama : ");
-    nama = stdin.readLineSync();
+    nama = stdin.readLineSync(); // data input dimasukan ke variable nama
+    
+    // Memasukan jumlah mata kuliah
     stdout.write("Masukan Jumlah Mata Kuliah : ");
-    int? jumlahMataKuliah = int.parse(stdin.readLineSync()!);
-
+    int? jumlahMataKuliah = int.parse(stdin.readLineSync()!); // Konversi dari String ke int
+    
+    // Deklarasi ulang atau pengosongan data yang sudah ada
+    namaMatkul = new Map();
+    
+    //Looping jumlah mata kuliah
     for(int j = 0; j < jumlahMataKuliah; j++) {
+      // Memasukan nama mata kuliah
       stdout.write("Masukan Mata Kuliah : ");
-      namaMatkul.add({stdin.readLineSync()});
+      namaMatkul[j] = stdin.readLineSync(); // data input dimasukan ke variable namaMatkul dengan index iterasi yang ditentukan looping
     }
-    print("--------------------------------");
   
     dataMahasiswa[i] = {
       'nim': nim,
       'nama': nama,
       'matkul': namaMatkul,
-    };
+    }; // Penyimpanan variable nim, nama, dan namaMatkul ke dalam variable dataMahasiswa dengan index iterasi yang ditentukan looping
 
     print("--------------------------------");
   }
-
-  print(dataMahasiswa);
+  
+  print("--------------------------------");
+  print(dataMahasiswa); // Menampilkan data yang disimpan di variable dataMahasiswa
 }
 
 void main() {
